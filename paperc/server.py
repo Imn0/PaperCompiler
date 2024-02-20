42507637e0a0f1f5987c032649d51728f62073e1
Krzysztof <krzysztof.jan.andrzejewski@gmail.com>
2024-02-20 15:36:16 +0100

    added compile / ocr option

--- paperc/server.py
+++ paperc/server.py
@@ -38,13 +38,69 @@ def get_test():
 
     return jsonify({'reply': test})
 
+@app.route('/compile', methods=['POST'])
+def get_compile():
 
-@app.route('/test_empty', methods=['GET'])
+    code= request.json.get('code')
+    if code is None:
+        return jsonify({'error': 'No code provided'})
+    print(code)
+
+    # code = unidecode(code)
+    code = code.encode('utf-8', 'ignore')
+    out = run_code(f'{code}', 54)
+    test = {
+        "out": out  
+    }
+
+    return jsonify({'reply': test})
+
+
+
+@app.route('/ocr', methods=['GET'])
+def get_ocr():
+     # Get the 'name' parameter from the query string
+
+    img = request.files.get('image', None)
+
+    image = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Morph open to remove noise and invert image
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+    invert = 255 - opening
+
+    recognized_text = pytesseract.image_to_string(invert)
+    # Print the final string
+    result_ascii = unidecode(recognized_text)
+
+    test = {
+        "ocr": result_ascii,
+    }
+
+    return jsonify({'reply': test})
+
+
+
+
+@app.route('/test_empty', methods=['POST'])
 def get_test_empty():
+
+    data = request.json  # Assuming the data is sent as JSON
+
+    # Process the data (for demonstration, just echoing back)
+    processed_data = {'received_data': data}
+
+    print(data)
+
     test = {
         "name": "aa",
-        "ocr": "aa",
-        "out": "aa"
+        "ocr": "dummy ocr",
+        "out": "dummy output"
     }
 
     return jsonify({'reply': test})
